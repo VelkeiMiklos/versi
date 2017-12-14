23ae933e0fa5f1bbafc5d9813ae44565e7309b32
VelkeiMiklos <velkei18@gmail.com>
2017-12-14 20:24:15 +0100

    VC-s

---
+++ versi/View/RepoCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoCell.swift
+//  versi
+//
+//  Created by Velkei Miklós on 2017. 12. 14..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ versi/View/SearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCell.swift
+//  versi
+//
+//  Created by Velkei Miklós on 2017. 12. 14..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
