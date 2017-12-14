8dcc9608cf3371fa04f36835fb9d7aa8338389aa
VelkeiMiklos <velkei18@gmail.com>
2017-12-14 18:57:19 +0100

    Initial Commit

---
+++ versi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  versi
+//
+//  Created by Velkei Miklós on 2017. 12. 14..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
